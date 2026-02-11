8cce372a7aeac43ecb1e4028bbb1f31861c204fd
Jakub Kopras <g00403547@atu.ie>
2026-02-11 21:44:39 +0000

    imports and security variable for dependencies.py

---
+++ backend/app/api/dependencies.py
@@ -0,0 +1,13 @@
+from fastapi import Depends, HTTPException, status
+from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
+from sqlalchemy.orm import Session
+
+from app.core.database import get_db
+from app.core.security import decode_access_token
+from app.models.database import User, UserRole
+from app.models.schemas import TokenData
+
+
+security = HTTPBearer()
+
+
